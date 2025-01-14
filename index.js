a0f0a59ff1eb5a794df5aedaf46b101d0bcb565b
Mohd Sahil <sahilbrother2005@gmail.com>
2025-01-14 11:42:51 +0530

    added signup and signin

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const path = require('path');
-const { auth,JWT_SECRET } = require('./auth')
-const { UserModel,TodoModel } = require('./db')
+const mongoose = require('mongoose');
+const { auth, JWT_SECRET } = require('./auth')
+const { UserModel, TodoModel } = require('./db')
+
+mongoose.connect("mongodb+srv://sahil2005:N8WJYc1k39faUGgL@cluster0.ti11j.mongodb.net/todos-week7")
 
 const app = express();
 app.use(express.json());
 
-app.post('/signup',function(req,res){
+app.post('/signup',async function(req,res){
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
 
+    await UserModel.create({
+        email:email,
+        password: password,
+        name: name
+    })
+
+    res.json({
+        message: "You are signed up"
+    })
 })
 
-app.post('/signup',function(req,res){
+app.post('/signin',async function(req,res){
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    const response = await UserModel.findOne({
+        email:email,
+        password:password
+    });
+
+    if(response){
+        const token = jwt.sign({
+            id: response._id.toString()
+        },JWT_SECRET);
+        res.json({
+            message: "you are signed in",
+            token: token
+        })
+    } else{
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
     
 })
